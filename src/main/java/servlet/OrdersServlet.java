e125171bb13b36b27d32ccfaf4aaf6b6f9fb9cf7
kaande <kaande@ttu.ee>
2020-10-17 00:31:55 +0300

    3

--- src/main/java/servlet/OrdersServlet.java
+++ src/main/java/servlet/OrdersServlet.java
@@ -33,7 +33,7 @@ public class OrdersServlet extends HttpServlet{
                          HttpServletResponse resp) throws IOException {
         String id = req.getParameter("id");
         Global.printLine("Get with id: " + id);
-        // 2
+        // 3
         // var ctx = new AnnotationConfigApplicationContext(Config.class, PostgresDataSource.class);
         ConfigurableApplicationContext ctx = (ConfigurableApplicationContext) getServletContext().getAttribute(Global.CTX);
 
