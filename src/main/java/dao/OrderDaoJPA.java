56a232e195c87c23f6290cfb9078cfc110a395b3
oskar-anderson <andersonkarloskar@gmail.com>
2023-04-11 18:53:23 +0300

    remove leftover duplicate code

--- src/main/java/dao/OrderDaoJPA.java
+++ src/main/java/dao/OrderDaoJPA.java
@@ -50,40 +50,4 @@ public class OrderDaoJPA {
       em.remove(order);
       return true;
    }
-
-
-
-   public List<UserJPA> findUsers() {
-      return em.createQuery("select " +
-              "o from UserJPA o").getResultList();
-   }
-
-   public UserJPA findUserByUsername(String username) {
-      TypedQuery<UserJPA> query = em.createQuery(
-              "select o from UserJPA o " +
-                      "where o.username = :username",
-              UserJPA.class);
-      query.setParameter("username", username);
-      return query.getSingleResult();
-   }
-
-   @Transactional
-   public UserJPA insertUser(UserJPA user) {
-      if (user.getUsername() == null) {
-         em.persist(user);
-      } else {
-         em.merge(user);
-      }
-      return user;
-   }
-
-
-   public boolean deleteUser(String username) {
-      UserJPA user = em.find(UserJPA.class, username);
-      if (user == null) {
-         return false;
-      }
-      em.remove(user);
-      return true;
-   }
 }
